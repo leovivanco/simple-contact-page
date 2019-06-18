495ba463842bec56c2c3589819a0f1a752df7323
Leonardo Vivanco <leorvivanco@gmail.com>
2019-06-18 21:48:27 +0100

    Build netlify

--- build/precache-manifest.b2257da6c877e54fb1e6f44b81f6b60d.js
+++ build/precache-manifest.b2257da6c877e54fb1e6f44b81f6b60d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5bb8b188453a7b5fd841c6e75dbf5aa9",
+    "revision": "761688b6046762beb28929920f378082",
     "url": "/index.html"
   },
   {
-    "revision": "0d7bbbe7107a511c38d2",
+    "revision": "8b6bbe9dc96673812fde",
     "url": "/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "f7d22ea9ab715528bfc4",
-    "url": "/static/css/main.5a1d0aff.chunk.css"
+    "revision": "83922de196d7b3f67054",
+    "url": "/static/css/main.2f06951f.chunk.css"
   },
   {
-    "revision": "0d7bbbe7107a511c38d2",
-    "url": "/static/js/2.5911238c.chunk.js"
+    "revision": "8b6bbe9dc96673812fde",
+    "url": "/static/js/2.9a013b10.chunk.js"
   },
   {
-    "revision": "f7d22ea9ab715528bfc4",
-    "url": "/static/js/main.4baee5a1.chunk.js"
+    "revision": "83922de196d7b3f67054",
+    "url": "/static/js/main.81c2f51c.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b349df837eed34cf192ded2dd53407bd.js"
+  "/precache-manifest.b2257da6c877e54fb1e6f44b81f6b60d.js"
 );
 
 self.addEventListener('message', (event) => {
