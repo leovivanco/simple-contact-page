1f64196877a5f2e903c2e1aacdcde2627e31b428
Leonardo Vivanco <leorvivanco@gmail.com>
2019-06-18 19:20:51 +0100

    Add build production

---
+++ build/precache-manifest.b349df837eed34cf192ded2dd53407bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb8b188453a7b5fd841c6e75dbf5aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d7bbbe7107a511c38d2",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f7d22ea9ab715528bfc4",
+    "url": "/static/css/main.5a1d0aff.chunk.css"
+  },
+  {
+    "revision": "0d7bbbe7107a511c38d2",
+    "url": "/static/js/2.5911238c.chunk.js"
+  },
+  {
+    "revision": "f7d22ea9ab715528bfc4",
+    "url": "/static/js/main.4baee5a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aabc2ddfde8c33f983ef49f8d0fa9921",
+    "url": "/static/media/WeCast_D.aabc2ddf.png"
+  },
+  {
+    "revision": "f6a6d1b60bcb365d0e28a8990766a05f",
+    "url": "/static/media/ico-global.f6a6d1b6.svg"
+  },
+  {
+    "revision": "fd80b020bcdbf86b700aae76f8e40ba1",
+    "url": "/static/media/img-tv-2.fd80b020.png"
+  }
+]);
