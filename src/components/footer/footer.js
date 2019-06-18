0459fadaea7f5c39817aa678f235fe876a66b8f1
Leonardo Vivanco <leorvivanco@gmail.com>
2019-06-18 21:34:28 +0100

    Fix variable

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,20 +3,18 @@ import { Row, Col } from 'reactstrap';
 import Localization from './localization';
 import NavFooter from './navFooter';
 import './footer.scss';
-export default class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer">
-        <Row>
-          <Col md="3">
-            <Localization />
-          </Col>
-          <Col md="9">
-            {this.props.menu}
-            <NavFooter />
-          </Col>
-        </Row>
-      </footer>
-    );
-  }
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <Row>
+        <Col md="3">
+          <Localization />
+        </Col>
+        <Col md="9">
+          <NavFooter />
+        </Col>
+      </Row>
+    </footer>
+  );
 };
--- src/components/header/logo/logo.js
+++ src/components/header/logo/logo.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logoImg from '../../../assets/img/WeCast_D.png';
-export default class Logo extends Component {
-  render() {
-    return <a href="https://business.wetek.com/wecast/" rel="noopener noreferrer" target="_blank"  className="img-logo">
-      <img src={logoImg} alt="" className="logo img-fluid" />
-    </a>
-  }
+export default function Logo() {
+  return <a href="https://business.wetek.com/wecast/" rel="noopener noreferrer" target="_blank" className="img-logo">
+    <img src={logoImg} alt="Logo wetek" className="logo img-fluid" />
+  </a>
 }
